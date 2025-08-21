3fc3cbcd368c200b4dceb5a5927eb42742a545b6
jwilliams-elastic <james.williams@elastic.co>
2025-08-21 18:22:11 -0400

    minor tweaks to aututune workflow and ltr model.

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,7 +2,6 @@
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
-import { weatherWorkflow } from './workflows/weather-workflow';
 import { elasticsearchSetupWorkflow } from './workflows/elasticsearch-setup-workflow';
 import { searchAutotuneWorkflow } from './workflows/search-autotune-workflow';
 import { homeSearchAgent } from './agents/home-search-agent';
@@ -15,7 +14,7 @@ import { elasticsearchSearchTool } from './tools/elasticsearch-search-tool';
 logger.info("Mastra runtime started");
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow, elasticsearchSetupWorkflow, searchAutotuneWorkflow },
+  workflows: { elasticsearchSetupWorkflow, searchAutotuneWorkflow },
   agents: { homeSearchAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
